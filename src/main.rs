f14d1f9da9cd5f13094a7444355fceef42298705
Lee Jia Keat <leejiakeat@gmail.com>
2024-04-13 10:48:54 +0800

    mushroom

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use bevy::{prelude::*, utils::HashMap, window::PrimaryWindow};
+use bevy::{prelude::*, transform::commands, utils::HashMap, window::PrimaryWindow};
 use std::ops::Index;
 
-const GLOBAL_SCALE: f32 = 2.0;
+const GLOBAL_SCALE: f32 = 1.0;
 const TILE_SIZE: f32 = 64.0;
+const MUSHROOM_SPEED: f32 = 10.0;
 
 #[derive(Eq, Hash, PartialEq)]
 enum ImageType {
@@ -50,6 +51,15 @@ struct GameCamera;
 struct MushroomBase;
 
 #[derive(Component)]
+struct Mushroom;
+
+#[derive(Component)]
+struct MushroomSummonManager {
+    timer: f32,
+    interval: f32,
+}
+
+#[derive(Component)]
 struct Ground;
 
 fn load_assets_system(mut image_manager: ResMut<ImageManager>, asset_server: Res<AssetServer>) {
@@ -83,6 +93,72 @@ fn load_assets_system(mut image_manager: ResMut<ImageManager>, asset_server: Res
             height: 0,
         },
     );
+}
+
+fn setup_ui_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font_handle = asset_server.load("fonts/Roboto-Regular.ttf");
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::SpaceAround,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: Color::rgb(0.1, 0.5, 0.1).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Button 1",
+                        TextStyle {
+                            font: font_handle.clone(),
+                            font_size: 40.0,
+                            // Alpha channel of the color controls transparency.
+                            color: Color::rgba(1.0, 1.0, 1.0, 0.2),
+                        },
+                    ));
+                });
+
+            // Button with a different color,
+            // to demonstrate the text looks different due to its transparency.
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: Color::rgb(0.5, 0.1, 0.5).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Button 2",
+                        TextStyle {
+                            font: font_handle.clone(),
+                            font_size: 40.0,
+                            // Alpha channel of the color controls transparency.
+                            color: Color::rgba(1.0, 1.0, 1.0, 0.2),
+                        },
+                    ));
+                });
+        });
 }
 
 fn setup_system(
@@ -139,9 +215,47 @@ fn setup_system(
         },
         MushroomBase,
     ));
+
+    commands.spawn(MushroomSummonManager {
+        timer: 0.0,
+        interval: 2.0,
+    });
 }
 
-fn test() {}
+fn mushroom_summon_system(
+    mut commands: Commands,
+    image_manager: Res<ImageManager>,
+    q_mushroom_base: Query<&Transform, With<MushroomBase>>,
+    mut q_summon_manager: Query<&mut MushroomSummonManager>,
+) {
+    let mushroom_sprite = &image_manager[ImageType::Mushroom];
+    let mushroom_base_position = q_mushroom_base.single().translation;
+
+    let mut mushroom_summon_manager = q_summon_manager.single_mut();
+
+    if mushroom_summon_manager.timer <= 0.0 {
+        mushroom_summon_manager.timer = mushroom_summon_manager.interval;
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(mushroom_base_position.x, mushroom_base_position.y, 0.0),
+                    scale: (Vec3::splat(GLOBAL_SCALE)),
+                    ..default()
+                },
+                texture: mushroom_sprite.handle(),
+                ..default()
+            },
+            Mushroom,
+        ));
+    }
+}
+
+fn mushroom_attack_system(mut q_mushroom: Query<&mut Transform, With<Mushroom>>, time: Res<Time>) {
+    q_mushroom.for_each_mut(|mut transform| {
+        transform.translation.x += MUSHROOM_SPEED * time.delta_seconds();
+    });
+}
 
 fn main() {
     App::new()
@@ -150,7 +264,7 @@ fn main() {
             images: HashMap::new(),
         })
         .add_systems(PreStartup, load_assets_system)
-        .add_systems(Startup, setup_system)
-        .add_systems(Update, test)
+        .add_systems(Startup, (setup_system, setup_ui_system))
+        .add_systems(Update, (mushroom_summon_system, mushroom_attack_system))
         .run();
 }
